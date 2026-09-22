eadc6eaff565a972d44b8962f09edb522edd97ff
Kurapati Sreesahasra <262394506+sreesahasrakurapati@users.noreply.github.com>
2026-09-22 15:10:51 +0530

    Actual code

--- Day-02/main.py
+++ Day-02/main.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-
+from pydantic import BaseModel 
 app = FastAPI()
+
 @app.get("/")
 def home():
-    return {"messege" : "Enterprise IT Desk"}
-db ={
-    1 : {"id": 1,"Title":"Computer is not ON",
-         "description":"power button is not working",
-         "category":"Hardware",
-         "status":"NEW"},
-    2 : {"id": 2,"Title":"Internet is not working.",
-         "description":"WIFI Problem",
-         "category":"Hardware",
-         "status":"NEW"}
+    return {"message" : "Enterprise IT Service Desk"}
+
+db = {
+    1: {"id" : 1, "title" : "Computer is not on",
+        "decsription" : "Power button is not working",
+        "category" : "Hardware", "status" : "NEW"},
+    2: {"id" : 2, "title" : "Internet is not working",
+        "decsription" : "wi-fi problem",
+        "category" : "Hardware", "status" : "NEW"}
 }
 
 class TicketCreate(BaseModel):
@@ -30,16 +29,29 @@ def ticket_read_all():
     return list(db.values())
 
 @app.get("/tickets/{id}")
-def ticket_read_all(id : int):
+def ticket_read_by_id(id:int):
     if id not in db:
-        raise HTTPException(detail = "Ticket Not Found",status_code=404)
+        raise HTTPException(detail= "Ticket not Found", status_code=404)
     return db[id]
 
-@app.post("/tickets",status_code=201,response_model=TicketResponse)
+
+@app.post("/tickets", status_code=201, response_model=TicketResponse)
+
 def ticket_create(ticket_payload : TicketCreate):
     new_id= max(db.keys(), default = 0) + 1
     db[new_id]= {"id" : new_id, **ticket_payload.model_dump()}
     return db[new_id]
 
+@app.put("/tickets/{id}", response_model=TicketResponse)
+def tickets_update(id: int, payload: TicketCreate):
+    if id not in db:
+        raise HTTPException(detail= "Ticket Not Found", status_code=404)
+    db[id] = {"id" : id, **payload.model_dump()}
+    return db[id]
 
-
+@app.delete("/tickets/{id}")
+def tickets_delete(id: int):
+    if id not in db:
+        raise HTTPException(detail="Ticket Not Found", status_code=404)
+    del db[id]
+    return {"message" : "Ticket Deleted Successfully"}
